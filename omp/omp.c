8f8ff82efd1d80ef8a7c6f4af4aba1f9c6fa6298
Norbert Ligas <norbert221198@gmail.com>
2021-12-12 15:07:23 +0100

    try with omp

--- omp/omp.c
+++ omp/omp.c
@@ -1,29 +1,48 @@
-#include<omp.h>
-#include<stdio.h>
-#include<math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <math.h>
 
-#include"my_timers.h"
+#include "my_timers.h"
+#include "../assets/image.xbm"
 
-#define NBR_OF_ELEMENTS 50000
+#define NBR_OF_ELEMENTS image_width * image_height / 3
+// #define NBR_OF_ELEMENTS image_width * image_height
+#define histogram_size 0xff + 1
+
+
+static unsigned long histogram[histogram_size];
+unsigned long i; // iterator
+
+
+void print_histogram()
+{
+	if(!histogram)
+	{
+		printf("Histogram has no values!");
+		return;
+	}	
+	
+	printf("Histogram values:\n");
+
+	for (i = 0; i < histogram_size; ++i)
+	{
+		printf("%lu, ", histogram[i]);
+	}
+}
 
 int main()
 {
-	long i,j;
-	float a[NBR_OF_ELEMENTS];
-	float p;
-	
 	omp_set_num_threads(4);
- 	
+
 	start_time();
- 	
-	#pragma omp parallel for
- 	for( i=0; i<NBR_OF_ELEMENTS; i++)
+
+	#pragma omp parallel for schedule(guided)
+	for (i = 0; i < NBR_OF_ELEMENTS; ++i)
 	{
- 		a[i]=0;
- 		for(j=0; j<i; j++)
- 			a[i]+=(float)j/(float)i;
- 	}
- 	
+		++histogram[image_bits[i]];
+	}
+
 	stop_time();
- 	print_time("Elapsed:");
+	print_time("Elapsed:");
 }
+
